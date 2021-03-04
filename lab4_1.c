eb0519870679639adbeb87094ad7b3fd3eb175a4
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-04 15:00:49 +0700

    1

---
+++ lab4_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j,m=0,mn=2000;
+    int b,c,sum;
+    scanf("%d",&n);
+    int a[n][3];
+    for (i=0;i<n;i++){
+        for (j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+}
