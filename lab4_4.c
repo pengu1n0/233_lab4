f15b5e60b29f0cc1a1e2f514d4007b92c9a4b785
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-03-04 17:02:15 +0700

    แก้

--- lab4_4.c
+++ lab4_4.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include<string.h>
 #include<ctype.h>
-int main() {
+int main() 
+{
   char n[1000] , g[11][5]={"i","of","the","on","at","for", "with", "a", "an" , "in" ,"and"} ;
   char *m;
   int a=0,b,j,i=0,k=0;
@@ -13,3 +14,22 @@ int main() {
             i++;
         }
   }
+  if(i!=0)
+  {
+       m= strtok(n," ");
+  while(m != NULL)
+    {
+        for(b=0;b<11;b++)
+          if(strcmp(m,g[b]))
+            k++;
+        if(k==11||a==0)
+         {
+             printf("%c",toupper(m[0]));
+         }
+
+    m = strtok(NULL," ");
+    a++, k=0;
+   }
+    }
+return 0;
+}
